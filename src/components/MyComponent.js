8b952fcab00b1a8d15a4fae37fe12f5930f2f79a
pratima-olixlab <olixlab.22@gmail.com>
2024-04-24 14:35:46 +0530

    some changes

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,1 @@
-import React from 'react';
-const MyComponent=(props)=> {
-  const {label} = props;
-  return (
-    <div>
-      <button>{label}</button>
-    </div>
-  );
-}
-export default MyComponent;
+hello
